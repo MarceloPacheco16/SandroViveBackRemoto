53c9ed2f55538c95d2d1207f747cd60fedf7aa4b
MarceloPacheco16 <marcelop639@gmail.com>
2024-11-03 17:45:19 -0300

    Ignorar Archivos, Comando al Iniciar Servidor Web, Configuracion en Django y Mas...
    Establecer Direccion de Ubicacion de Entorno Virtual (en VSC)
    
    En ".gitignore":
    ignoramos los archivos que no son necesarios o son datos sensibles para que PRODUCCION
    
    En "Procfile":
    es un archivo de configuración utilizado por plataformas de despliegue.
    web: Ejecutará el siguiente codigo como el proceso web principal de la aplicación.
    python manage.py migrate:
    Este comando se utiliza para aplicar las migraciones de la base de datos (aseguramos de que la estructura de la base de datos esté actualizada)
    python manage.py collectstatic --noinput:
    Este comando se utiliza para recopilar todos los archivos estáticos (CSS, JavaScript, imágenes, etc.) de las aplicaciones y de los directorios de archivos estáticos en un solo lugar, que se especifica por la configuración STATIC_ROOT.
    gunicorn proyecto_django.wsgi:application --bind 0.0.0.0:$PORT:
    Este comando inicia el servidor gunicorn, que servirá tu aplicación Django.
    --bind 0.0.0.0:$PORT indica que gunicorn escuchará en todas las interfaces de red (0.0.0.0) en el puerto que se especifica mediante la variable de entorno $PORT. Esto es necesario para que la aplicación sea accesible desde el exterior.
    
    En "setting.py":
    Importamos la dependencia "dotenv" para cargar las VARIABLES DE ENTORNO. con el metodo "load_dotenv()".
    Luego las establecemos estas variables en este archivo.
    
    Con "STATIC ROOT" establecemos la ubicación en tu sistema de archivos donde Django almacenará todos los archivos estáticos recogidos por el comando "python manage.py collectstatic". Al ejecutar collectstatic, Django recopila todos los archivos estáticos de tus aplicaciones y los coloca en el directorio especificado por STATIC_ROOT. Esto facilita la configuración del servidor web para servir estos archivos estáticos de manera eficiente.
    Su carpeta se ignora por ".gitignore" es una práctica recomendada para evitar redundancias, mantener el repositorio ligero y limpio, y optimizar el flujo de trabajo de desarrollo.
    
    En "requirements.txt" se cargaron nuevas dependencias:
    "dotenv":  Gestiona y carga variables de entorno desde un archivo .env, ayudando a mantener configuraciones sensibles fuera del código
    "gunicorn": Es un servidor de aplicaciones WSGI que permite ejecutar aplicaciones web de manera eficiente y escalable en producción.

--- proyecto_django/proyecto_django/proyecto_django/settings.py
+++ proyecto_django/proyecto_django/proyecto_django/settings.py
@@ -12,21 +12,28 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)o17+#qxfu2@hi8ngen8fw1)e+2x5c^p*jl(7!pa$2635qt-a'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-i)o17+#qxfu2@hi8ngen8fw1)e+2x5c^p*jl(7!pa$2635qt-a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -141,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
