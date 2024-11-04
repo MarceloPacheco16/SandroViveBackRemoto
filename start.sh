60b44ad7d6328b5bccca13b41a1842791368d5ca
MarceloPacheco16 <marcelop639@gmail.com>
2024-11-04 00:18:40 -0300

    Navegar a la Ubicacion del Proyecto de Django para Railway y Variable de Entorno SETTINGS
    En "start.sh":
    Debes asegurarte de que el "start.sh" navegue correctamente al directorio donde está "manage.py".
    Diferencias de Entorno: Tu máquina local tiene una estructura de directorios que es diferente a la que tendrá tu aplicación una vez desplegada. En tu caso, el comando "cd /app/proyecto_django/proyecto_django" es para el entorno de producción y debe reflejar cómo Railway organiza los archivos después del despliegue.
    
    En ".env":
    Si tu proyecto tiene configuraciones separadas para desarrollo y producción (por ejemplo, settings_dev.py y settings_prod.py), DJANGO_SETTINGS_MODULE te permite cambiar entre ellas fácilmente estableciendo su valor en el entorno. Así puedes definir en tu archivo .env la configuración que quieras activar, por ejemplo:
    # Para desarrollo local
    DJANGO_SETTINGS_MODULE='proyecto_django.settings_dev'
    
    # Para producción
    DJANGO_SETTINGS_MODULE='proyecto_django.settings_prod'
    
    Al definir DJANGO_SETTINGS_MODULE en .env, no necesitas cambiar nada más en tu código base; Django utilizará esta variable automáticamente. Si necesitas cambiar la configuración (por ejemplo, entre desarrollo y producción), solo tienes que modificar el valor de DJANGO_SETTINGS_MODULE en tu archivo .env, sin tocar el código de configuración en settings.py.

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Navegar al directorio donde se encuentra manage.py
+cd /app/proyecto_django/proyecto_django
+
 # Realizar migraciones de base de datos
 python manage.py migrate
 
@@ -7,4 +10,4 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Iniciar el servidor usando Gunicorn
-exec gunicorn proyecto_django.proyecto_django.wsgi:application --bind 0.0.0.0:$PORT --log-file -
+exec gunicorn proyecto_django.wsgi:application --bind 0.0.0.0:$PORT --log-file -
