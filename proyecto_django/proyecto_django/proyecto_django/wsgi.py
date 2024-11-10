9ef1509b001386b654918315fac133e07ec63c4c
MarceloPacheco16 <marcelop639@gmail.com>
2024-11-10 12:26:22 -0300

    Configurar servidor con Waitress en producción
    Procfile:
    Modificamos el nombre del script para ejecutar el servidor de "start.sh" a "start-server.sh"
    
    wsgi.py:
    Configuramos el archivo para usar Waitress
    
    start-server.sh:
    Reemplazamos la llamada de "python manage.py runserver" con el comando que utiliza Waitress para servir la aplicación "python proyecto_django/wsgi.py".
    Eliminar la definición de PORT en el script start-server.sh, ya que Railway ya le asignará un puerto automáticamente. Si el puerto no está definido, Railway lo asignará a través de la variable de entorno PORT.

--- proyecto_django/proyecto_django/proyecto_django/wsgi.py
+++ proyecto_django/proyecto_django/proyecto_django/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from waitress import serve
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_django.settings')
 
 application = get_wsgi_application()
+
+# Usar Waitress para servir la aplicación en producción
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=8000)
--- start-server.sh
+++ start-server.sh
@@ -4,8 +4,8 @@ set -x  # Muestra cada comando antes de ejecutarlo
 
 echo "Iniciando script..."
 
-# Establecer el puerto si no está definido
-export PORT=${PORT:-8000}
+# # Establecer el puerto si no está definido
+# export PORT=${PORT:-8000}
 
 # Navegar al directorio del proyecto donde se encuentra manage.py
 cd ./proyecto_django/proyecto_django || { echo "Error al cambiar de directorio"; exit 1; }
@@ -19,8 +19,12 @@ python manage.py migrate || { echo "Error al ejecutar migraciones"; exit 1; }
 echo "Recopilando archivos estáticos..."
 python manage.py collectstatic --noinput || { echo "Error al recopilar archivos estáticos"; exit 1; }
 
-# Iniciar el servidor de desarrollo de Django
-echo "Iniciando el servidor de desarrollo..."
-exec python manage.py runserver 0.0.0.0:${PORT} || { echo "Error al iniciar el servidor"; exit 1; }
+# # Iniciar el servidor de desarrollo de Django
+# echo "Iniciando el servidor de desarrollo..."
+# exec python manage.py runserver 0.0.0.0:${PORT} || { echo "Error al iniciar el servidor"; exit 1; }
+
+# Iniciar el servidor con Waitress
+echo "Iniciando el servidor con Waitress..."
+exec python proyecto_django/wsgi.py || { echo "Error al iniciar el servidor"; exit 1; }
 
 echo "Script finalizado."
