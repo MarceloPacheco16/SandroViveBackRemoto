93e9c242f05fd431a09fa123c6ee9d8df8af218e
MarceloPacheco16 <marcelop639@gmail.com>
2024-11-29 02:17:52 -0300

    Archivos Estaticos a Cloudinary
    En "setting.py":
    Modificamos la configuracion para probar si ahora funciona la configuracion de los archivos estaticos.

--- proyecto_django/proyecto_django/proyecto_django/settings.py
+++ proyecto_django/proyecto_django/proyecto_django/settings.py
@@ -118,7 +118,6 @@ CORS_ALLOW_CREDENTIALS = True
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Añade Whitenoise aquí
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -219,15 +218,8 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
     'API_KEY': CLOUDINARY_API_KEY,
     'API_SECRET': CLOUDINARY_API_SECRET,
+    'STATIC_IMAGES_FOLDER': 'staticfiles',  # Carpeta raíz para archivos estáticos en Cloudinary
 }
-
-# # Configuración de Cloudinary
-# cloudinary.config(
-#     cloud_name=CLOUDINARY_CLOUD_NAME,
-#     api_key=CLOUDINARY_API_KEY,
-#     api_secret=CLOUDINARY_API_SECRET,
-# )
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -237,16 +229,11 @@ STATIC_URL = '/static/'  # URL base para archivos estáticos
 STATICFILES_DIRS = [BASE_DIR / 'static']  # Carpeta local donde están los archivos estáticos
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta donde collectstatic recopila archivos
 
-# # Usando Cloudinary para los archivos estáticos
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# Usando Cloudinary para los archivos estáticos
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 # # Para archivos estáticos, usa el almacenamiento estándar o Whitenoise
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# # Usando Whitenoise para servir archivos estáticos locales en desarrollo
-# # Esto solo afectará a los archivos estáticos locales, no los de Cloudinary
-# if not DEBUG:
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Archivos multimedia
 MEDIA_URL = '/media/'  # URL base para archivos multimedia
